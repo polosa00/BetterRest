ddbf3e36b942a517763d538f1ad2dce678f892c2
polosa00 <i@apolochanin.ru>
2023-10-30 20:50:16 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Александр Полочанин on 30.10.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
